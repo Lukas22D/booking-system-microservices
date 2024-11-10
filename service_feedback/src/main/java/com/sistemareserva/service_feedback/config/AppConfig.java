2143a430e8698eb59c698085f40af0724eec48d4
Lucas Silva Rocha <luk.look.s.rocha@gmail.com>
2024-11-10 17:03:16 -0300

    Refatoração no docker-compose para otimizar configuração dos serviços em contêiner

--- service_feedback/src/main/java/com/sistemareserva/service_feedback/config/AppConfig.java
+++ service_feedback/src/main/java/com/sistemareserva/service_feedback/config/AppConfig.java
@@ -6,7 +6,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
