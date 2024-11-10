ea2d67d3ef94c21b5d51265c8b2be1d656af31e1
Lucas Silva Rocha <luk.look.s.rocha@gmail.com>
2024-11-09 22:52:11 -0300

    Refatora código para suportar variáveis de ambiente e adiciona containerização da aplicação

--- service_reservas/src/main/java/com/sistemareserva/service_reservas/client/feignClient/QuartosClient.java
+++ service_reservas/src/main/java/com/sistemareserva/service_reservas/client/feignClient/QuartosClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.sistemareserva.service_reservas.client.feignClient.dto.QuartosReponse;
 
-@FeignClient(name = "quartos", url = "http://localhost:8081/quarto")
+@FeignClient(name = "quartos", url = "service-quartos:8081/quarto")
 public interface QuartosClient {
 
     @GetMapping("/{id}")
