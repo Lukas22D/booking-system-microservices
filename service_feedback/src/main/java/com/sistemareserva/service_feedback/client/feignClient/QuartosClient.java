7b8abd42f56bbba423cef2413e9bab2b1a11f30e
Lucas Silva Rocha <luk.look.s.rocha@gmail.com>
2024-11-11 20:24:05 -0300

    Refatoração do código para utilização de variáveis de ambiente e containerização do serviço em uma imagem Docker

--- service_feedback/src/main/java/com/sistemareserva/service_feedback/client/feignClient/QuartosClient.java
+++ service_feedback/src/main/java/com/sistemareserva/service_feedback/client/feignClient/QuartosClient.java
@@ -2,7 +2,7 @@ package com.sistemareserva.service_feedback.client.feignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "quartos-service", url = "http://localhost:8081/quarto")
+@FeignClient(name = "quartos-service", url = "service-quartos:8081/quarto")
 public interface QuartosClient  {
     
 }
