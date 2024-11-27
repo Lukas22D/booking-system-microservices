7a4aef46f4a3e72cdebd8416d8d6a7ba7e6f84a9
Lucas Silva Rocha <luk.look.s.rocha@gmail.com>
2024-11-27 16:05:06 -0300

    Refat. na camada de user cases, encapsulamento das regras de negocios com as entidades do dominio

--- service_payment/src/main/java/com/sistemareserva/service_payment/domain/service/core/TransactionService.java
+++ service_payment/src/main/java/com/sistemareserva/service_payment/domain/service/core/TransactionService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.sistemareserva.service_payment.domain.handler.error.OrderCreateException;
 import com.sistemareserva.service_payment.domain.service.TransactionInterface;
-import com.sistemareserva.service_payment.infra.adapters.paypal.dto.OrderResponse;
 import com.sistemareserva.service_payment.infra.adapters.persistence.db.model.enums.TransactionStatus;
 import com.sistemareserva.service_payment.domain.entity.OrderEntity;
 import com.sistemareserva.service_payment.domain.entity.TransactionEntity;
@@ -42,10 +41,13 @@ public class TransactionService implements TransactionInterface {
         CompletableFuture<List<OrderEntity>> order = CompletableFuture
                 .completedFuture(reservasClient.getReserva(idHospede))
                 .thenCompose(or -> {
+                    // Verifica se a reserva foi encontrada
                     if (or == null) {
+                        // Loga e lança exceção caso não encontre a reserva
                         logger.error("Reserva não encontrada para o hospede: " + idHospede);
                         throw new EntityNotFoundException("Reserva não encontrada para o hospede: " + idHospede);
                     }
+                    // Cria o pedido de forma assíncrona
                     return paymenteInterface.createOrder(or)
                             .exceptionally(e -> {
                                 logger.error("Erro ao criar pedido: " + e.getMessage());
@@ -53,20 +55,16 @@ public class TransactionService implements TransactionInterface {
                             });
                 });
 
-
-        // Inicia a operação de salvar transações de forma assíncrona
-        order.thenAcceptBoth(order, (response, orderList) -> {
-            saveTransaction(reservasList, response.id())
-                    .exceptionally(e -> {
-                        logger.error("Erro ao salvar transações: " + e.getMessage());
-                        throw new RuntimeException("Falha ao salvar transações", e);
-                    });
+        // Processa o pedido e salva a transação de forma assíncrona
+        return order.thenApply(o -> {
+            // Chama saveTransaction de forma assíncrona
+            saveTransaction(o);
+            // Retorna o LinkOrder do primeiro pedido
+            return o.get(0).getLinkOrder();
         });
-        // Retorna o link de aprovação do pedido
-        return order.thenApply
     }
 
-    public CompletableFuture<Void> saveTransaction(List<OrderEntity> request, String orderId) {
+    public CompletableFuture<Void> saveTransaction(List<OrderEntity> request) {
 
         return CompletableFuture.runAsync(() -> {
             List<TransactionEntity> transactions = request.stream()
@@ -74,7 +72,7 @@ public class TransactionService implements TransactionInterface {
                             null,
                             Long.valueOf(reserva.getIdReserva()),
                             Integer.parseInt(reserva.getQuantidadeDias()),
-                            orderId,
+                            reserva.getIdOrder(),
                             Long.valueOf(reserva.getIdHospede()),
                             new BigDecimal(reserva.getValorTotal())))
                     .collect(Collectors.toList());
